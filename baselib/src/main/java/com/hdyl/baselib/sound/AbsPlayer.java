569608bd6c9aacf50b2503ce6fc3f96098df509d
15310872381 <15310872381>
2017-07-21 22:38:23 +0800

    1

---
+++ baselib/src/main/java/com/hdyl/baselib/sound/AbsPlayer.java
@@ -0,0 +1,35 @@
+package com.hdyl.baselib.sound;
+
+import android.content.Context;
+import android.support.annotation.RawRes;
+
+import com.hdyl.baselib.base.App;
+
+import java.util.HashMap;
+
+/**
+ * 音乐播放器，共同的基类
+ * Created by Administrator on 2017/5/14.
+ */
+
+public abstract class AbsPlayer {
+
+
+    protected final Context mContext = App.getContext();
+    protected final HashMap<Integer, Integer> soundMap = new HashMap<>();
+
+    public AbsPlayer() {
+        initDatas();
+    }
+
+
+    protected void initDatas() {
+    }
+
+    public abstract void initRawRes(int soundFlag, @RawRes int rawRes);
+
+    public abstract void play(int soundFlag);
+
+    public abstract void release();
+
+}
---
+++ baselib/src/main/java/com/hdyl/baselib/sound/SoundPlayer.java
@@ -0,0 +1,44 @@
+package com.hdyl.baselib.sound;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.support.annotation.RawRes;
+
+/**音效播放器
+ * Created by Administrator on 2017/5/14.
+ */
+
+public class SoundPlayer extends AbsPlayer {
+
+
+    SoundPool soundPool;
+
+    @Override
+    protected void initDatas() {
+        soundPool = new SoundPool(8, AudioManager.STREAM_MUSIC, 0);
+    }
+
+    @Override
+    public void initRawRes(int soundFlag, @RawRes int rawRes) {
+        soundMap.put(soundFlag, soundPool.load(mContext, rawRes, 1));
+    }
+
+    public void play(int soundFlag) {
+        AudioManager mgr = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        float streamVolumeCurrent = mgr.getStreamVolume(AudioManager.STREAM_MUSIC);
+        float streamVolumeMax = mgr.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        float volume = streamVolumeCurrent / streamVolumeMax;
+        Integer soundID = soundMap.get(soundFlag);
+        if (soundID != null) {
+            soundPool.play(soundID, volume, volume, 1, 0, 1.0f);
+        }
+    }
+
+    public void release() {
+        if (soundPool != null) {
+            soundPool.release();
+            soundPool = null;
+        }
+    }
+}
