a34a9467fd285644fe57b0d01be8328491a2149e
liugd <1203416812@qq.com>
2017-07-25 09:46:14 +0800

    1

--- app/src/main/java/com/hdyl/gamebase/MainActivity.java
+++ app/src/main/java/com/hdyl/gamebase/MainActivity.java
@@ -2,12 +2,23 @@ package com.hdyl.gamebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.hdyl.baselib.utils.ToastUtils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.textView1).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        ToastUtils.makeTextAndShow("aa", R.drawable.ic_launcher);
     }
 }
