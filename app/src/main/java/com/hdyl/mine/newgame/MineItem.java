1140da4f5bcbca12063972d5c66e34351fad6253
15310872381 <986850427@qq.com>
2018-07-11 08:41:10 +0800

    扫雷优化

--- app/src/main/java/com/hdyl/mine/newgame/MineItem.java
+++ app/src/main/java/com/hdyl/mine/newgame/MineItem.java
@@ -9,17 +9,45 @@ public class MineItem {
     public int var;//
     public int flag;//标记
 
+
     public final static int VAR_OPEN = 0;
-    public final static int VAR_COVER = 1 << 5;
-    public final static int VAR_FLAG = 2 << 5;
+    public final static int VAR_COVER = 1;
+    public final static int VAR_FLAG = 1 << 1 | VAR_COVER;//标记也算一种cover
 
-    public final static int VAR_MASK = VAR_COVER;
+    //
+//    public final static int VAR_MASK = VAR_COVER;
+//
+//    //0~12
+//
+//
+    public boolean isOpen() {
+        return isFlagValue(VAR_OPEN);
+    }
 
-    //0~12
 
+    /***
+     * 切换标记和cover
+     */
+    public void toggleCoverFlag() {
+        if (!isOpen()) {
+            if (isCover()) {
+                flag = VAR_FLAG;
+            } else {
+                flag = VAR_COVER;
+            }
+        }
+    }
 
     public boolean isCover() {
-        return (VAR_MASK & var) == VAR_COVER;
+        return isFlagValue(VAR_COVER);
+    }
+
+    private boolean isFlagValue(int var) {
+        return (flag & var) == var;
+    }
+
+    public boolean isFlag() {
+        return isFlagValue(VAR_FLAG);
     }
 
 //    public int getValue(){
