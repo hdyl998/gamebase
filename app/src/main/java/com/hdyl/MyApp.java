0c07350b9f667ed7aa56c34a11607d306785d403
liugd@qqshidao.com <liugd@qqshidao.com>
2019-08-05 11:24:19 +0800

    1111

--- app/src/main/java/com/hdyl/MyApp.java
+++ app/src/main/java/com/hdyl/MyApp.java
@@ -4,6 +4,7 @@ import com.hdyl.baselib.appconfig.AppConfigFactory;
 import com.hdyl.baselib.appconfig.TestAppConfig;
 import com.hdyl.baselib.base.App;
 import com.umeng.analytics.MobclickAgent;
+import com.umeng.commonsdk.UMConfigure;
 
 /**
  * Created by liugd on 2017/9/6.
@@ -14,6 +15,9 @@ public class MyApp extends App {
     public void onCreate() {
         super.onCreate();
         AppConfigFactory.setAppConfig(new TestAppConfig());
-        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
+//        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
+
+        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, null);
+        MobclickAgent.openActivityDurationTrack(false);
     }
 }
